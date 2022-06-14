d41fbfcb17c82c4d0b3ea57cfd38933f761a8b8d
Faiz Muzaki <34159640+faizmuzaki@users.noreply.github.com>
2022-06-14 21:51:24 +0700

    Update antreanApotek.cpp
    Change Ideas

--- antreanApotek.cpp
+++ antreanApotek.cpp
@@ -24,14 +24,14 @@ int main(){
 	int menu,pilihObat;
 	string nama;
 	queue<string> q;
-	string obat[] = {"Paracetamol","Dekongestan","Ibuprofen","Asam Mefenamat","Antalgin"};
-	int jumlahObat,hargaObat[] = {19000,59900,26008,15000,21000};
+	string obat[] = {"Classic Pancake","Croffle","Nachos","Potato Wedges","Loco Moco"};
+	int jumlahObat,hargaObat[] = {30000,45000,50000,35000,45000};
 	int arrSize = sizeof(obat)/sizeof(obat[0]);
 do{
 	cout << "-----------------------------------------------------------------" << endl;
-	cout << "                         Menu Apotek Melati                      "<< endl;
+	cout << "                         Menu Cafe                      "<< endl;
  	cout << "-----------------------------------------------------------------" << endl;
-	cout<<"1.Ambil Antrean\n2.Beli Obat\n3.Cek Antrean\n4.Exit\nMasukkan Menu : ";
+	cout<<"1.Ambil Antrean\n2.Pilih Menu\n3.Cek Antrean\n4.Exit\nMasukkan Menu : ";
 	cin>>menu;	
 	switch(menu){
 		case 1:
